20fd6a85d591421b8a50cafc822001b8e1a4fde1
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-03-03 21:25:49 +0200

    Initial commit

---
+++ app/src/main/java/ua/turskyi/gdgfinder/home/HomeFragment.kt
@@ -0,0 +1,36 @@
+package ua.turskyi.gdgfinder.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import ua.turskyi.gdgfinder.R
+import ua.turskyi.gdgfinder.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = HomeFragmentBinding.inflate(inflater)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner,
+            Observer<Boolean> { shouldNavigate ->
+                if (shouldNavigate == true) {
+                    val navController = binding.root.findNavController()
+                    navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                    viewModel.onNavigatedToSearch()
+                }
+            })
+        return binding.root
+    }
+}
